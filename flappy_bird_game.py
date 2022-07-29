ee74f92872175b364bbb01e8012e22bd2b739df0
Yossi Tzeel-Krupp <yossitz1302@gmail.com>
2022-07-29 15:38:48 +0300

    initial commit

---
+++ flappy_bird_game.py
@@ -0,0 +1,14 @@
+from turtle import window_height, window_width
+import pygame
+import neat
+import time
+import os
+import random
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+
+BIRD_IMGS = [pygame.transform.scale2x()]
+
+
